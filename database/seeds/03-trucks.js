db5d8f880f3eee5f30eeb1094248054950d0da4e
Jorge Manzur <46169161+manzur1990@users.noreply.github.com>
2020-08-25 19:25:30 -0500

    Update 03-trucks.js

--- database/seeds/03-trucks.js
+++ database/seeds/03-trucks.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('trucks').del()
+  return knex('trucks').delete()
     .then(function () {
       // Inserts seed entries
       return knex('trucks').insert([
